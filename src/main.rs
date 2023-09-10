2e3b411aa360778ef66bd0f7e76e50a312b50707
jbruws <vanmain.22@gmail.com>
2023-09-10 16:16:22 +0300

    finished submessage/topic/ system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_files;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use chrono::{DateTime, FixedOffset, Local, NaiveDateTime};
 use regex::Regex;
 use serde::Deserialize;
@@ -7,8 +7,11 @@ use std::sync::Mutex;
 use tokio;
 use tokio_postgres;
 
-// WARNING: none of this works yet.
-// Topics/submessages are still WIP.
+enum BoardMessageType {
+    Message,       // messages on main page
+    ParentMessage, // parent message on topic pages
+    Submessage,    // submessages on topic pages
+}
 
 #[derive(Deserialize)]
 struct MsgForm {
@@ -28,6 +31,8 @@ struct ApplicationState {
     last_message_id: Mutex<i64>,
 }
 
+// i should probably make those functions async as well
+
 fn get_time(since_epoch: i64) -> String {
     let offset = FixedOffset::east_opt(3 * 3600).unwrap(); // +3 offset
     let naive = NaiveDateTime::from_timestamp_opt(since_epoch, 0).unwrap(); // UNIX epoch to datetime
@@ -35,11 +40,28 @@ fn get_time(since_epoch: i64) -> String {
     dt[..dt.len() - 7].to_string() // 7 was chosen experimentally
 }
 
-fn filter_string(inp_string: &String) -> String { // removing html tags
-    let filter = Regex::new(r##"<.*?>"##).unwrap(); 
+fn format_into_html(
+    message_type: BoardMessageType,
+    id: &i64,
+    time: &str,
+    author: &str,
+    msg: &str,
+) -> String {
+    let f_result = match message_type {
+        BoardMessageType::Message => format!(include_str!("../message_templates/message.html"), id, time, author, id, id, msg),
+        BoardMessageType::ParentMessage => format!(include_str!("../message_templates/parent_message.html"), time, author, id, id, msg),
+        BoardMessageType::Submessage => format!(include_str!("../message_templates/submessage.html"), id, time, author, id, msg),
+    };
+    f_result
+}
+
+fn filter_string(inp_string: &String) -> String {
+    // removing html tags
+    let filter = Regex::new(r##"<.*?>"##).unwrap();
     String::from(filter.replace_all(inp_string.as_str(), ""))
 }
 
+#[get("/")]
 async fn main_page(data: web::Data<ApplicationState>) -> impl Responder {
     let mut counter = data.counter.lock().unwrap();
     *counter += 1;
@@ -48,109 +70,22 @@ async fn main_page(data: web::Data<ApplicationState>) -> impl Responder {
 
     if messages.len() != 0 {
         for t in (&*messages).into_iter().rev() {
-            let string_time = get_time(t.1);
             inserted_msg.push_str(
-                format!(
-                    "<div class=\"message\" id={}> <p class=\"message_header\">{} <br> {} <br> {}</p><br> {}</div>\n",
-                    &t.0, // message id
-                    &string_time, // time of creation
-                    &t.2, // author
-                    &t.0,
-                    &t.3 // message contents
+                format_into_html(
+                    BoardMessageType::Message, 
+                    &t.0, 
+                    &get_time(t.1), 
+                    &t.2, 
+                    &t.3
                 )
-                .as_str(),
+                .as_str()
             );
         }
     }
     HttpResponse::Ok().body(format!(include_str!("../html/index.html"), inserted_msg))
 }
 
-#[post("/topic/{message_num}")]
-async fn message_page(
-    data: web::Data<ApplicationState>,
-    info: web::Path<PathInfo>,
-    form: web::Form<MsgForm>,
-) -> impl Responder {
-    let client = data.db_client.lock().unwrap();
-    let mut head_msg: String;
-    let head_msg_data = client
-        .query_one(
-            "SELECT * FROM messages WHERE msgid=($1)",
-            &[&info.message_num],
-        )
-        .await;
-    if let Ok(d) = head_msg_data {
-        head_msg = format!("<div id=\"head_message\" id={}> <p class=\"message_header\">{} <br> {} <br> {}</p><br> {}</div>\n",
-            d.get::<usize, i64>(0), // message id
-            get_time(d.get(1)), // time of creation
-            d.get::<usize, String>(2), // author
-            d.get::<usize, i64>(0),
-            d.get::<usize, String>(3) // message contents
-        );
-    } else {
-        return HttpResponse::Ok().body("404 No Such Message Found");
-    }
-    let mut inserted_submsg = String::from("");
-    let mut submessage_counter = 0;
-    for row in client
-        .query(
-            "SELECT * FROM submessages WHERE parent_msg=($1)",
-            &[&info.message_num],
-        )
-        .await
-        .unwrap()
-    {
-        submessage_counter += 1;
-        let string_time = get_time(row.get(1));
-        inserted_submsg.push_str(
-                format!(
-                    "<div class=\"submessage\" id={}> <p class=\"message_header\">{} <br> {} <br> {}</p><br> {}</div>\n",
-                    &submessage_counter, // ordinal number
-                    &string_time, // time of creation
-                    &row.get::<usize, String>(2), // author
-                    &submessage_counter,
-                    &row.get::<usize, String>(3) // message contents
-                )
-                .as_str(),
-            );
-    }
-
-    HttpResponse::Ok().body(format!(
-        include_str!("../html/topic.html"),
-        &info.message_num.to_string(),
-        head_msg,
-        inserted_submsg,
-        &info.message_num.to_string(), // for the form
-    ))
-}
-
-async fn process_submessage_form(
-    data: web::Data<ApplicationState>,
-    info: web::Path<PathInfo>,
-) -> impl Responder {
-    let client = data.db_client.lock().unwrap();
-
-    // getting time
-    let since_epoch: i64 = match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH)
-    {
-        Ok(n) => n.as_secs().try_into().unwrap(),
-        Err(_) => 1,
-    };
-
-    // if fits, push new message into DB and vector
-    if form.author.len() < 254 && form.message.len() < 4094 {
-        let filtered_author = filter_string(&form.author);
-        let filtered_msg = filter_string(&form.message);
-
-        client
-            .execute(
-                "INSERT INTO submessages(parent_msg, time, author, msg) VALUES (($1), ($2), ($3), ($4))",
-                &[&info.message_num, &since_epoch, &filtered_author, &filtered_msg],
-            )
-            .await; // i'll just pretend this `Result` doesn't exist
-    }
-}
-
+#[post("/")]
 async fn process_form(
     form: web::Form<MsgForm>,
     data: web::Data<ApplicationState>,
@@ -186,6 +121,97 @@ async fn process_form(
             .await; // i'll just pretend this `Result` doesn't exist
     }
     web::Redirect::to("http://192.168.0.110:8080").see_other()
+}
+
+#[get("/topic/{message_num}")]
+async fn message_page(
+    data: web::Data<ApplicationState>,
+    info: web::Path<PathInfo>,
+    //form: web::Form<MsgForm>,
+) -> impl Responder {
+    let client = data.db_client.lock().unwrap();
+    let mut head_msg: String;
+    let head_msg_data = client
+        .query_one(
+            "SELECT * FROM messages WHERE msgid=($1)",
+            &[&info.message_num],
+        )
+        .await;
+    if let Ok(d) = head_msg_data {
+        head_msg = format_into_html(
+            BoardMessageType::ParentMessage,
+            &d.get::<usize, i64>(0),    // message id
+            &get_time(d.get(1)),        // time of creation
+            &d.get::<usize, String>(2), // author
+            &d.get::<usize, String>(3), // message contents
+        );
+    } else {
+        return HttpResponse::Ok().body("404 No Such Message Found");
+    }
+    let mut inserted_submsg = String::from("");
+    let mut submessage_counter = 0;
+    for row in client
+        .query(
+            "SELECT * FROM submessages WHERE parent_msg=($1)",
+            &[&info.message_num],
+        )
+        .await
+        .unwrap()
+    {
+        submessage_counter += 1;
+        let string_time = get_time(row.get(1));
+        inserted_submsg.push_str(
+                format_into_html(
+                    BoardMessageType::Submessage,
+                    &submessage_counter, // ordinal number
+                    &string_time, // time of creation
+                    &row.get::<usize, String>(2), // author
+                    &row.get::<usize, String>(3) // message contents
+                )
+                .as_str(),
+            );
+    }
+
+    HttpResponse::Ok().body(format!(
+        include_str!("../html/topic.html"),
+        &info.message_num.to_string(),
+        head_msg,
+        inserted_submsg,
+        &info.message_num.to_string(),
+    ))
+}
+
+#[post("/topic/{message_num}")]
+async fn process_submessage_form(
+    data: web::Data<ApplicationState>,
+    form: web::Form<MsgForm>,
+    info: web::Path<PathInfo>,
+) -> impl Responder {
+    let client = data.db_client.lock().unwrap();
+
+    // getting time
+    let since_epoch: i64 = match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH)
+    {
+        Ok(n) => n.as_secs().try_into().unwrap(),
+        Err(_) => 1,
+    };
+
+    // if fits, push new message into DB and vector
+    if form.author.len() < 254 && form.message.len() < 4094 {
+        let filtered_author = filter_string(&form.author);
+        let filtered_msg = filter_string(&form.message);
+        client
+            .execute(
+                "INSERT INTO submessages(parent_msg, time, author, submsg) VALUES (($1), ($2), ($3), ($4))",
+                &[&info.message_num, &since_epoch, &filtered_author, &filtered_msg],
+            )
+            .await; // i'll just pretend this `Result` doesn't exist
+    }
+    web::Redirect::to(format!(
+        "http://192.168.0.110:8080/topic/{}",
+        info.message_num
+    ))
+    .see_other()
 }
 
 #[actix_web::main]
@@ -233,9 +259,12 @@ async fn main() -> std::io::Result<()> {
             .app_data(count.clone())
             .service(actix_files::Files::new("/html", "./html"))
             .service(message_page)
-            .route("/", web::get().to(main_page))
-            .route("/process_form", web::post().to(process_form))
-            //.route("/topic/{message_num}/process_submessage_form", web::post().to(process_submessage_form))
+            .service(process_form)
+            .service(process_submessage_form)
+            .service(main_page)
+        //.route("/", web::get().to(main_page))
+        //.route("/process_form", web::post().to(process_form))
+        //.route("/topic/{message_num}/process_submessage_form", web::post().to(process_submessage_form))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
