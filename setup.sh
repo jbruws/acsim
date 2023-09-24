54ae3d307d4638e5716ae5629293ea96b9c52dc9
jbruws <vanmain.22@gmail.com>
2023-09-24 19:54:46 +0300

    Included table creation in setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+# Setting up environment for the server
 mkdir ./user_images
 touch config.json
 echo '
@@ -10,3 +11,20 @@ echo '
 	"bind_to_one_ip": false
 }
 ' > config.json
+
+# Creating database tables
+echo 'CREATE TABLE IF NOT EXISTS messages (
+		msgid SERIAL PRIMARY KEY,
+		time BIGINT NOT NULL,
+		author VARCHAR (255) NOT NULL,
+		latest_submsg BIGINT,
+		image VARCHAR (128),
+		msg VARCHAR (4096) NOT NULL
+	);
+	CREATE TABLE IF NOT EXISTS submessages (
+		parent_msg BIGINT NOT NULL,
+		time BIGINT NOT NULL,
+		author VARCHAR (255) NOT NULL,
+		image VARCHAR (128),
+		submsg VARCHAR (4096) NOT NULL
+	);' | psql -U postgres -d qibe_db;
